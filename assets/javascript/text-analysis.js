b9b84a867f75cd6c97cac1c3cac1fc596fad7139
Dan Proctor <dproc96@gmail.com>
2019-07-06 16:04:18 -0400

    tweaked formula by comparing multiple movies

--- assets/javascript/text-analysis.js
+++ assets/javascript/text-analysis.js
@@ -1,12 +1,54 @@
-var testString = nlp("I'm looking for a movie that is sad but also heartwarming and sweet and will make me cry. Something I can watch with my kids but with snappy dialogue.")
+var testString = nlp("I'm looking for a movie that is sad but also heartwarming and sweet and will make me cry. Something I can watch with my kids but with dialogue.")
 
-var testReview = `This is a very clever animated story that was a big hit, and justifiably so. It had a terrific sequel and if a third film came out, that would probably be a hit, too.
+var testReview = `You can judge the quality of each successive instalment of the petrolhead Fast & Furious franchise by its opening action sequence. Here, the action revs up with a car chase around the streets of Havana. One of the first Hollywood movies to shoot in Cuba, F&F8 effectively uses one of the most photogenic cities on Earth as a display cabinet for vintage hotrods and buttocks. The race is between a turbo-boosted Cuban loan shark and Dom Toretto (Vin Diesel) in a disintegrating clown car. Powered by distilled testosterone alone, Dom wins the race, driving backwards, consumed in a fireball, without brakes. The writing is scorched into the tarmac: this episode is aiming for new levels of high-octane silliness.
 
-When this came out, computer technology just was beginning to strut its stuff. Man, this looked awesome. Now, it's routine because animation, which took a giant leap with this movie, has made a lot more giant strides.
+There is no shortage of muscle battling for alpha-male supremacy. In Vin Diesel, Jason Statham and Dwayne Johnson, the film has three of the shiniest, baldest, most artificially bronzed men in Hollywood. When they start to butt heads, it’s like watching a human conker game. Of the three, it’s Statham who swaggers away with the movie, thanks to an audacious fight sequence accessorised with a gurgling baby in a travel seat and a cameo from British grand dame Helen Mirren as his tea-swilling mum.
 
-The humor in here, however, is what made this so popular. There are tons of funny lines, issued by characters voiced by Tom Hanks, Tim Allen, Jim Varney, Don Rickles, Wallace Shawn and John Ratzenberger, among others. As good as Hanks is as "Woody" and Allen as "Buzz Armstrong," I think the supporting characters just about stole the show: Mr. Potato Head, Slinky, Rex the dinosaur, etc.
 
-Multiple viewings don't diminish the entertainment, either. There are so many things to catch, audibly and visually, that you always seem to discover something new. The colors in here are beautiful, too. This is a guaranteed "winner" as is the sequel.`
+Sign up to our Film Today email
+ Read more
+The baby is a key plot device. The child-menacing antagonist is super-hacker Cipher (Charlize Theron, kitted out with stringy blond dreadlocks, which are meant to suggest “cyberpunk princess of darkness” but look a bit too “gap yah in Goa” for comfort). Cipher is stockpiling WMDs, but since she has the power to hack into anything with a chip – including, obviously, most cars – it’s not entirely clear why she needs the weapons. A geeks-will-inherit-the-earth cyberpocalypse hardly requires a carjacked nuclear submarine to trigger panic. It just needs someone to turn off the internet for a while.
+
+Ultimately, you would have more luck reassembling a working vehicle after Cipher’s remotely hacked zombie-car pile-up than you would piecing together a logical plot. But then logic is not really the strong point of a series that features Diesel walking unscathed and in slow-mo from a screaming pile of twisted metal at least five times per film.`
+
+var testReview2 = `The second-greatest headline in the history of the Onion is:
+
+Millions of pet owners demand 
+to know: "Who's a good boy?"
+
+This line is not frequently used in the Grogan household. There is a reason for that. Marley is not a good boy. I'd love to have a dog around the house, but not Marley. We have, you know, stuff we like. Books, dishes, tables, chairs, rugs, curtains. You know how it is. Marley considers such objects to be food, playthings or enemies.
+
+There was a real Marley. He belonged to John and Jennifer Grogan, and was the subject of a 2005 best seller that has been adapted into this film. I hope the book earned enough to pay for Marley's overhead. Marley has the behavior pattern of a manic wrecking crew and the appetite of a science-fiction monster, but you gotta love him. At least, the Grogans gotta love him. They may be as crazy as their dog. Here is a useful lesson. When you go to the pet lady, and she shows you a group of Labrador puppies and one is cheaper than all the others, this is not the time to go bargain-hunting.
+
+"Marley & Me" is a cheerful family movie about a young couple starting out in life with a new house, new jobs, a new dog and then three children, who the dog doesn't eat, or the movie wouldn't be rated PG. Owen Wilson and Jennifer Aniston play the Grogans as brave and resourceful. Every couple has to survive ups and downs in their marriage, but Louis XVI and Marie Antoinette might be alive today if they'd adopted Marley, he had eaten the crown jewels, and they'd fled the palace and abdicated.
+
+You would think the dog would supply the playful, upbeat elements in this movie. Not exactly. Marley supplies the Sturm und Drang, 24-7. It is John Grogan's professional life that supplies the fantasy. He gets a job as a cub reporter on a newspaper and is soon ordered by his editor (Alan Arkin) to become a columnist. Nothing doing! he says. He'd rather cover school board meetings and sewer inspectors. Arkin counters: "I'll double your salary!"
+
+In today's newspaper world, this plays like escapist porn. Grogan would be ordered to carry a route on his way to work, and Arkin would be replaced by Uncle Scrooge. But Grogan makes the canny decision to write a column about the dog, and it is a great success. Soon the column and the dog are beloved. Marley becomes as useful to Grogan as Slats Grobnik was to Mike Royko: always good for a column on a slow day. Come to think of it, Marley has all the earmarks of having been trained by Slats, starting out as a puppy by eating barstools and spittoons.
+
+This may be the first family film I've seen that will frighten more adults than children. The Marley kids, Conor, Patrick and Colleen, all love Marley. Their parents are appalled. At one point, Jennifer actually despairs and tells John that either the dog goes or she does. No, actually, she doesn't force him to choose. She's outta there. But she relents and returns to the doggie from hell. The thing about Marley, see, is that he has an uncanny way of knowing exactly when to pause in eating the garage and gaze soulfully upon his masters with unconditional love.
+
+When Marley is not on the screen, Wilson and Aniston demonstrate why they are gifted comic actors. They have a relationship that's not too sitcomish, not too sentimental, mostly smart and realistic. That's because she plays a newspaper reporter, too. Marley would have been a welcome break after a day in the riotous city rooms of the good old days. In today's city rooms, reporters hide in their cubicles praying to escape extermination. I say lock Scrooge in a cage and throw in Marley.`
+
+testReview3 = `'The Notebook" cuts between the same couple at two seasons in their lives. We see them in the urgency of young romance, and then we see them as old people, she disappearing into the shadows of Alzheimer's, he steadfast in his love. It is his custom every day to read to her from a notebook that tells the story of how they met and fell in love and faced obstacles to their happiness. Sometimes, he says, if only for a few minutes, the clouds part and she is able to remember who he is and who the story is about.
+
+We all wish Alzheimer's could permit such moments. For a time, in the earlier stages of the disease, it does. But when the curtain comes down, there is never another act and the play is over. "The Notebook" is a sentimental fantasy, but such fantasies are not harmful; we tell ourselves stories every day, to make life more bearable. The reason we cried during "Terms of Endearment" was not because the mother was dying, but because she was given the opportunity for a dignified and lucid parting with her children. In life it is more likely to be pain, drugs, regret and despair.
+
+The lovers are named Allie Nelson and Noah Calhoun, known as Duke. As old people they're played by Gena Rowlands and James Garner. As young people, by Rachel McAdams and Ryan Gosling. The performances are suited to the material, respecting the passion at the beginning and the sentiment at the end, but not pushing too hard; there is even a time when young Noah tells Allie, "I don't see how it's gonna work," and means it, and a time when Allie gets engaged to another man.
+
+She's a rich kid, summering at the family's mansion in North Carolina. He's a local kid who works at the sawmill but is smart and poetic. Her parents are snobs. His father (Sam Shepard) is centered and supportive. Noah loves her the moment he sees her, and actually hangs by his hands from a bar on a Ferris wheel until she agrees to go out with him. Her parents are direct: "He's trash. He's not for you." One day her mother (Joan Allen) shows her a local working man, who looks hard-used by life, and tells Allie that 25 years ago she was in love with him. Allie thinks her parents do not love each other, but her mother insists they do; still, Allen is such a precise actress that she is able to introduce the quietest note of regret into the scene.
+
+The movie is based on a novel by Nicholas Sparks, whose books inspired "Message in a Bottle" (1999), unloved by me, and "A Walk to Remember" (2002), which was so sweet and positive it persuaded me (as did Mandy Moore as its star). Now here is a story that could have been a tearjerker, but -- no, wait, it is a tearjerker, it's just that it's a good one. The director is Nick Cassavetes, son of Gena Rowlands and John Cassavetes, and perhaps his instinctive feeling for his mother helped him find the way past soap opera in the direction of truth.
+
+Ryan Gosling has already been identified as one of the best actors of his generation, although usually in more hard-edged material. Rachel McAdams, who just a few months ago was the bitchy high school queen in "Mean Girls," here shows such beauty and clarity that we realize once again how actors are blessed by good material. As for Gena Rowlands and James Garner: They are completely at ease in their roles, never striving for effect, never wanting us to be sure we get the message. Garner is an actor so confident and sure that he makes the difficult look easy, and loses credit for his skill. Consider how simply and sincerely he tells their children: "Look, guys, that's my sweetheart in there." Rowlands, best-known for high-strung, even manic characters, especially in films by her late husband, here finds a quiet vulnerability that is luminous.
+
+The photography by mournful Robert Fraisse is striking in its rich, saturated effects, from sea birds at sunset to a dilapidated mansion by candlelight to the texture of Southern summer streets. It makes the story seem more idealized; certainly the retirement home at the end seems more of heaven than of earth.
+
+And the old mansion is underlined, too, first in its decay and then in its rebirth; young Noah is convinced that if he makes good on his promise to rebuild it for Allie, she will come to live in it with him, and paint in the studio he has made for her. ("Noah had gone a little mad," the notebook says.) That she is engaged to marry another shakes him but doesn't discourage him.
+
+We have recently read much about Alzheimer's because of the death of Ronald Reagan. His daughter Patti Davis reported that just before he died, the former president opened his eyes and gazed steadily into those of Nancy, and there was no doubt that he recognized her.
+
+Well, it's nice to think so. Nice to believe the window can open once more before closing forever.`
 
 var textParsed = {
     people: testString.people().out("array"),
@@ -20,29 +62,40 @@ for (let adjective of textParsed.adjectives) {
     adjectiveArray.push([adjective, 2])
     var maxscore
     $.ajax({
-        url: "http://api.datamuse.com/words?ml="+adjective,
+        url: "http://api.datamuse.com/words?ml="+adjective+"&topics=movie",
         method: "GET"
     }).then(function(response) {
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < response.length; i++) {
             let item = response[i]
             if (i === 0) {
                 maxscore = item.score
             }
             let score = item.score/maxscore
-            if (item.tags.includes("adj") && item.score > 2500) {
+            if (score > 0) {
                 adjectiveArray.push([item.word, score])
             }
         }
-        console.log(adjectiveArray)
     })
 }
 
 setTimeout(function() {
+    console.log(adjectiveArray)
     var score = 0;
+    var score2 = 0;
+    var score3 = 0;
     for (let adjective of adjectiveArray) {
         let adjectiveRegEx = new RegExp(adjective[0], "g");
         let count = (testReview.match(adjectiveRegEx) || []).length;
+        let count2 = (testReview2.match(adjectiveRegEx) || []).length;
+        let count3 = (testReview3.match(adjectiveRegEx) || []).length;
         score += count * adjective[1];
+        score2 += count2 * adjective[1];
+        score3 += count3 * adjective[1];
     }
+    score = score / (testReview.split(" ").length)
+    score2 = score2 / (testReview2.split(" ").length)
+    score3 = score3 / (testReview3.split(" ").length)
     console.log(score)
+    console.log(score2)
+    console.log(score3)
 }, 1000)
