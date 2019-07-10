34f0d88ca6579ce3230c6075e7b62de21d69df42
JasperJRoth <Jasper.J.Roth@gmail.com>
2019-07-09 21:12:45 -0400

    Added password logins

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,26 +1,5 @@
 
-var provider = new firebase.auth.GoogleAuthProvider();
-
 const siteAuth = {
-  signIn: function(){
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // The signed-in user info.
-        var user = result.user;
-  
-        console.log(`Signed in successfully as: ${user.email}`);
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-  
-        console(`Login failed with user: ${email} on auth provider ${credential}`);
-        console.log(`Error ${errorCode}: ${errorMessage}`);
-      });
-  },
   getUserData: async function(){
     var sendCall = firebase.functions().httpsCallable("getUserData");
   
@@ -38,5 +17,32 @@ const siteAuth = {
     }).catch(function(error) {
       throw new Error(`Error ${error.errorCode}: ${error.message}`);
     });
+  },
+  signUp: function(email, pass){
+    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+      throw new Error(`Error ${error.errorCode}: ${error.message}`);
+    });
+  },
+  signIn: function(method, email, pass){
+    switch(method){
+      case "email":
+          firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+            throw new Error(`Error ${error.errorCode}: ${error.message}`);
+          });
+        return;
+      case "google":
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).catch(function(error) {
+          throw new Error(`Error ${error.errorCode}: ${error.message}`);
+        });
+        return;
+    }
+  },
+  activeUser: function(){
+    return firebase.auth().currentUser;
+  },
+  signOut: function(){
+    firebase.auth().signOut();
   }
 }
